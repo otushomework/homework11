cf2e96e6b35429c3213bbc82da2f61e7a896de00
Vitaly Petrov <v31337@gmail.com>
2019-03-22 18:13:58 +0300

    Clear comments

--- bulkhandler.h
+++ bulkhandler.h
@@ -39,11 +39,6 @@ public:
 
         while (std::getline(fdStream, m_line))
         {
-            //remove white spaces
-            //m_line.erase(std::remove(m_line.begin(), m_line.end(), ' '), m_line.end());
-
-            //std::cout << "m_line " << m_line << std::endl;
-
             //buffering line, for example when client send symbols one by one
             if (m_line.length() > 0 && m_line[m_line.length() - 1] != '\n')
             {
@@ -55,8 +50,6 @@ public:
             m_lineBuffer.append(m_line);
             m_line = m_lineBuffer;
             m_lineBuffer.clear();
-
-            //std::cout << "m_line buffered " << m_line << " size " << m_line.size() << std::endl;
 
             if (m_line.size() == 0 || (m_line.length() == 1 && m_line[1] == '\n') )
                 continue;
